e076bec79ddf1ebff0d86a6880b017823ca90737
LiaOsorio86 <codigoenprueba@gmail.com>
2025-09-16 23:54:09 -0300

    Realizamos juego de piedra, papel, tijera en python

---
+++ piedra_papel_tijera.py
@@ -0,0 +1,28 @@
+nombrejugador1= input('Ingrese el Nombre del Jugador 1: ')
+nombrejugador2= input('Ingrese el Nombre del Jugador 2: ')
+
+seguir='s'
+
+while seguir.lower() == 's':
+    jugador1= input(f'{nombrejugador1}, que elegis? piedra - papel - tijera?: ')
+    jugador2= input(f'{nombrejugador2}, que elegis? piedra - papel - tijera?: ')
+
+    while jugador1==jugador2:
+        print ('Ha sido un empate, vuelvan a intentar')
+        jugador1= input(f'{nombrejugador1}, que elegis? piedra - papel - tijera?: ')
+        jugador2= input(f'{nombrejugador2}, que elegis? piedra - papel - tijera?: ')
+
+    if (jugador1 == 'piedra' and jugador2 == 'tijera') or\
+       (jugador1=='tijera' and jugador2=='papel')or\
+       (jugador1=='papel' and jugador2=='piedra'):
+            print (nombrejugador1, 'ha ganado')
+    else: 
+        print (nombrejugador2, 'ha ganado')
+        
+    seguir= input ('Seguimos jugando? S/N: ')
+
+print ('Ha finalizado el juego!')
+
+
+
+
